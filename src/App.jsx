cf3030fcd205afbd10702cddaae74b375ff29cc0
inf0saurus <infosaurus@gmail.com>
2025-07-10 15:51:26 -1000

    Have add local storage to save data in the event of a refresh.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
@@ -7,8 +7,13 @@ function App() {
   const [todoValue, setTodoValue] = useState('');
 
 
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify({ todos: newList }));
+  }
+
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo];
+    persistData(newTodoList);
     setTodos(newTodoList);
   }
 
@@ -16,6 +21,7 @@ function App() {
     const newTodoList = todos.filter((todo, todoIndex) => {
       return todoIndex !== index;
     })
+    persistData(newTodoList);
     setTodos(newTodoList);
   }
 
@@ -25,6 +31,17 @@ function App() {
     handleDeleteTodo(index);
   }
 
+  useEffect(() => {
+    if (!localStorage) {
+      return;
+    }
+    let localTodos = localStorage.getItem('todos');
+    if (!localTodos) {
+      return
+    }
+    localTodos = JSON.parse(localTodos).todos;
+    setTodos(localTodos);
+  }, [])
   return (
     <>
       <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos}/>
